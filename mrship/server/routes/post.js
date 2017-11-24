4a5b819f0d642625c02fd1d4e799ebc0beb858cd
dengzhou <dengzhou@sinovoice.com.cn>
2017-11-24 17:30:40 +0800

    word error

--- mrship/server/routes/post.js
+++ mrship/server/routes/post.js
@@ -2,7 +2,7 @@
  * Created by dengzhou on 2017/11/24.
  */
 //post.js
-modules.exports = function (req, res) {
+module.exports = function (req, res) {
     console.log(req.body.userName);
     console.log(req.body.password);
     res.json({code: 200, data: "this is a post request"});
