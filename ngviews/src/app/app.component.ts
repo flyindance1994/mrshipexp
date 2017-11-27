5d12658106c786d656e793440b6a80e67141ebeb
dengzhou <dengzhou@sinovoice.com.cn>
2017-11-27 10:25:07 +0800

    angualr init

---
+++ ngviews/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component,OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'app';
+  constructor(){}
+
+  msg:string="hello angular2";
+  msgList:string[]=['angular1','angular2','angular4'];
+
+  ngOnInit(){}
+
+  add(){
+    this.msgList.push('angular');
+  }
+}
